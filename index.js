7e079a0b962ead0880a0b642799e839c3ea0751a
Kirstie <kirstiewithrow@gmail.com>
2021-10-15 13:12:04 -0500

    completed 10.15

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-for (let age = 30; age < 40; age++) {
-    console.log(`I'm ${age} years old. Happy birthday to me!`);
-   
-  } 
- 
-  const gifts = ["teddy bear", "drone", "doll"];
-
-  function wrapGifts(gifts) {
-    for (let i = 0; i < gifts.length; i++) {
-      console.log(`Wrapped ${gifts[i]} and added a bow!`);
-      
-    }
-  
-    return gifts;
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
   }
-  
-  wrapGifts(gifts); 
+  return thankYouCards
+}
 
-  const name = ["Ada", "Brendan", "Ali"]
-  function writeCards(name) {
-      for(let i = 0; i< name.length; i++) {
-          console.log(`Thank you, ${name[i]}, for the wonderful birthday gift!`);
-      }
-      debugger;
-      return name;
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
   }
-  writeCards(name);
-
-  let countdown = 0;
-  while (countdown < 34) {
-      console.log(countdown++);
-  }
-  
+  console.log( startingNumber );
+}
